f56c348f4486e6f2c2b33f8cccb445e30df717a2
WhiteRaven45 <84709425+WhiteRaven45@users.noreply.github.com>
2022-01-19 14:51:50 +0300

    Проект - этап 2
    Завершено подключение базы данных

---
+++ Задание - Магазин косметики Красавицы (часть С#)/Мои результаты/BeautiesShopApp/BeautiesShopApp/AppDataFile/ConnectOdb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautiesShopApp.AppDataFile
+{
+    class ConnectOdb
+    {
+        public static BeautiesShopDBEntities conObj;
+    }
+}
--- Задание - Магазин косметики Красавицы (часть С#)/Мои результаты/BeautiesShopApp/BeautiesShopApp/MainWindow.xaml.cs
+++ Задание - Магазин косметики Красавицы (часть С#)/Мои результаты/BeautiesShopApp/BeautiesShopApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BeautiesShopApp.AppDataFile;
 
 namespace BeautiesShopApp
 {
@@ -23,6 +24,7 @@ namespace BeautiesShopApp
         public MainWindow()
         {
             InitializeComponent();
+            ConnectOdb.conObj = new BeautiesShopDBEntities();
         }
     }
 }
