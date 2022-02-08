529e3976f1ff711bed24143f5bf9c21fa8117b6b
WhiteRaven45 <84709425+WhiteRaven45@users.noreply.github.com>
2022-02-08 08:56:28 +0300

    Докинул файлы

---
+++ Задание - Магазин косметики Красавицы (часть С#, попытка на UTF)/Мои результаты/BeautiesShopApplication/BeautiesShopApplication/Pages/PageAddProduct.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BeautiesShopApplication.AppDataFile;
+using BeautiesShopApplication.Pages;
+
+namespace BeautiesShopApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddProduct.xaml
+    /// </summary>
+    public partial class PageAddProduct : Page
+    {
+        bool LogicRb = false;
+        public PageAddProduct()
+        {
+            InitializeComponent();
+            CmbxOwner.SelectedValuePath = "ID";
+            CmbxOwner.DisplayMemberPath = "Name";
+            CmbxOwner.ItemsSource = ClsDb.ent.Manufacturer.ToList();
+
+            if (RbActive.IsChecked != false)
+            {
+                LogicRb = true;
+            }
+            else
+            {
+                LogicRb = false;
+            }
+            if (RbNotActive.IsChecked != false)
+            {
+                LogicRb = false;
+            }
+            else
+            {
+                LogicRb = true;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product product = new Product()
+                {
+                    Title = TxtTitle.Text,
+                    Cost = Convert.ToDecimal(TxtCost.Text),
+                    Description = TxtDescription.Text,
+                    MainImagePath = TxtImage.Text,
+                    IsActive = LogicRb,
+                    Manufacturer = CmbxOwner.SelectedItem as Manufacturer
+                };
+
+                ConnectOdb.conObj.Product.Add(product);
+                ConnectOdb.conObj.SaveChanges();
+                MessageBox.Show("Данные успешно изменены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Задание - Магазин косметики Красавицы (часть С#, попытка на UTF)/Мои результаты/BeautiesShopApplication/BeautiesShopApplication/Pages/PageEditNew.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BeautiesShopApplication.AppDataFile;
+using BeautiesShopApplication.Pages;
+
+namespace BeautiesShopApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEditNew.xaml
+    /// </summary>
+    public partial class PageEditNew : Page
+    {
+        bool logicRb = false;
+        public PageEditNew(Product product)
+        {
+            InitializeComponent();
+            CmbxOwner.SelectedIndex = (int)product.ManufacturerID - 1;
+            CmbxOwner.SelectedValuePath = "ID";
+            CmbxOwner.DisplayMemberPath = "Name";
+            CmbxOwner.ItemsSource = ConnectOdb.conObj.Manufacturer.ToList();
+
+            ProductObj.ID = product.ID;
+            TxtTitle.Text = product.Title;
+            TxtCost.Text = Convert.ToString(product.Cost);
+            TxtImage.Text = product.MainImagePath;
+            TxtDescription.Text = product.Description;
+
+            if (product.IsActive != false)
+            {
+                RbActive.IsChecked = true;
+                logicRb = true;
+            }
+            else
+            {
+                RbNotActive.IsChecked = true;
+                logicRb = false;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IEnumerable<Product> products = ConnectOdb.conObj.Product.Where(x => x.ID == ProductObj.ID).AsEnumerable().Select(x =>
+                {
+                    x.Title = TxtTitle.Text;
+                    x.Cost = Convert.ToDecimal(TxtCost.Text);
+
+                    if (string.IsNullOrWhiteSpace(TxtCost.Text))
+                    {
+                        x.Description = "";
+                    }
+                    else
+                    {
+                        x.Description = TxtDescription.Text;
+                    }
+                    if (string.IsNullOrWhiteSpace(TxtImage.Text))
+                    {
+                        x.MainImagePath = "";
+                    }
+                    else
+                    {
+                        x.MainImagePath = TxtImage.Text;
+                    }
+
+                    x.IsActive = logicRb;
+                    x.ManufacturerID = (int)CmbxOwner.SelectedValue;
+                    return x;
+                });
+                foreach (Product prdct in products)
+                {
+                    ConnectOdb.conObj.Entry(prdct).State = System.Data.Entity.EntityState.Modified;
+                }
+                ConnectOdb.conObj.SaveChanges();
+                MessageBox.Show("Данные успешно изменены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message.ToString());
+            }
+        }
+    }
+}
